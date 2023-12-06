475cc6a8929ea6c30736e62e1de92aefb1e735da
albnsp <a.suprunova1@gmail.com>
2023-12-06 22:18:23 +0400

    add Task12.java

---
+++ src/Task12.java
@@ -0,0 +1,22 @@
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println("Задание: 12");
+        int[] array = {1, 2, 3, 2, 1}; // Инициализация массива
+
+        boolean isSymmetric = true; // Флаг для проверки симметричности
+
+        // Проверка симметричности массива
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                isSymmetric = false;
+                break;
+            }
+        }
+        // Вывод результата
+        if (isSymmetric) {
+            System.out.println("Yes"); // Симметричный массив
+        } else {
+            System.out.println("No"); // Несимметричный массив
+        }
+    }
+}
